52b4567ccac24f05c8064f48ad21d9254f4fe40c
Oleh <olehkavetskyi@gmail.com>
2023-02-03 19:08:20 +0200

    End of section 12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
+import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
+import { NgxSpinner } from 'ngx-spinner';
+import { NgxSpinnerModule } from 'ngx-spinner/public_api';
 
 @NgModule({
   declarations: [
@@ -23,6 +26,7 @@ import { ErrorInterceptor } from './core/interceptors/error.interceptor';
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
 })
--- src/app/shop/product-details/product-details.component.ts
+++ src/app/shop/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/models/product';
+import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
 
 @Component({
@@ -11,16 +12,25 @@ import { ShopService } from '../shop.service';
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
 
-  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute) {  }
+  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute,
+     private bcService: BreadcrumbService) {  
+      this.bcService.set('@productDetails', ' ')
+     }
 
   ngOnInit(): void {
     this.loadProduct();
   }
 
-  loadProduct() {
-    this.shopService.getProduct(this.activateRoute.snapshot.paramMap.get('id')).subscribe({
-      next: (product) => this.product = product,
-      error: (err) => console.log(err)
-    })
+  loadProduct() {//difference
+    const id = this.activateRoute.snapshot.paramMap.get('id');
+
+    if(id) 
+      this.shopService.getProduct(id).subscribe({
+        next: (product) => {
+          this.product = product;
+          this.bcService.set('@productDetails', product.name) 
+        },
+        error: (err) => console.log(err)
+      })
   }
 }
